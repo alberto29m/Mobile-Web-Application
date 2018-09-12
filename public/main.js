e92f1027eb5e3df529499d2f76ed3dd40745ab42
alberto rodriguez <alberto29m@gmail.com>
2018-09-12 17:05:54 +0200

    Web pages separated with differents divs

--- public/main.js
+++ public/main.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: "#app",
     data: {
         data1: [],
-        data2: []
+        data2: [],
+        sourceUrl: ""
 
     },
     created: function () {
@@ -24,6 +25,7 @@ var app = new Vue({
                 throw new Error(response.statusText);
             }).then(function (json) {
                 app.loader();
+                app.changeAllPagesToMenu();
                 app.data1 = json.gamesData;
                 console.log(app.data1);
                 app.data2 = json.allLocations;
@@ -52,11 +54,118 @@ var app = new Vue({
                 }
             }
         },
-        loader: function(){
-        var containerLoader = document.getElementById("containerLoader");
-//        setTimeout(function(){
+        loader: function () {
+            var containerLoader = document.getElementById("containerLoader");
             containerLoader.classList.add("cerrar");
-//        },);    
+            setTimeout(function () {
+                containerLoader.style.display = "none";
+                document.getElementById("preloader").style.display = "none";
+            }, 2000);
+        },
+        changeMenuToGames: function(){
+            var menu = document.getElementById("firstPage");
+            var gamePage=document.getElementById("gamesPage");
+            var gameDetailsPage = document.getElementById("gameDetailsPage");
+            var locationsPage = document.getElementById("locationsPage");
+            var mapPage = document.getElementById("locationMapPage");
+            var contactPage = document.getElementById("contactPage");
+            menu.style.display="none";
+            gameDetailsPage.style.display="none";
+            locationsPage.style.display="none";
+            mapPage.style.display="none";
+            contactPage.style.display="none";
+            gamePage.style.display="block";
+        },
+        changeMenuToLocations: function(){
+            var menu = document.getElementById("firstPage");
+            var gamePage=document.getElementById("gamesPage");
+            var gameDetailsPage = document.getElementById("gameDetailsPage");
+            var locationsPage = document.getElementById("locationsPage");
+            var mapPage = document.getElementById("locationMapPage");
+            var contactPage = document.getElementById("contactPage");
+            menu.style.display="none";
+            gameDetailsPage.style.display="none";
+            locationsPage.style.display="block";
+            mapPage.style.display="none";
+            contactPage.style.display="none";
+            gamePage.style.display="none";
+            
+        },
+        changeMenuToContact: function(){
+            var menu = document.getElementById("firstPage");
+            var gamePage=document.getElementById("gamesPage");
+            var gameDetailsPage = document.getElementById("gameDetailsPage");
+            var locationsPage = document.getElementById("locationsPage");
+            var mapPage = document.getElementById("locationMapPage");
+            var contactPage = document.getElementById("contactPage");
+            menu.style.display="none";
+            gameDetailsPage.style.display="none";
+            locationsPage.style.display="none";
+            mapPage.style.display="none";
+            contactPage.style.display="block";
+            gamePage.style.display="none";
+            
+        },
+        changeAllPagesToMenu: function(){
+            var menu = document.getElementById("firstPage");
+            var gamePage=document.getElementById("gamesPage");
+            var gameDetailsPage = document.getElementById("gameDetailsPage");
+            var locationsPage = document.getElementById("locationsPage");
+            var mapPage = document.getElementById("locationMapPage");
+            var contactPage = document.getElementById("contactPage");
+            menu.style.display="block";
+            gameDetailsPage.style.display="none";
+            locationsPage.style.display="none";
+            mapPage.style.display="none";
+            contactPage.style.display="none";
+            gamePage.style.display="none";
+        },
+        changeGameToGameDetails: function(){
+            var menu = document.getElementById("firstPage");
+            var gamePage=document.getElementById("gamesPage");
+            var gameDetailsPage = document.getElementById("gameDetailsPage");
+            var locationsPage = document.getElementById("locationsPage");
+            var mapPage = document.getElementById("locationMapPage");
+            var contactPage = document.getElementById("contactPage");
+            menu.style.display="none";
+            gamePage.style.display="none";
+            gameDetailsPage.style.display="block";
+            locationsPage.style.display="none";
+            mapPage.style.display="none";
+            contactPage.style.display="none";        
+        },
+        changeLocationsToLocationMap: function(){
+            var menu = document.getElementById("firstPage");
+            var gamePage=document.getElementById("gamesPage");
+            var gameDetailsPage = document.getElementById("gameDetailsPage");
+            var locationsPage = document.getElementById("locationsPage");
+            var mapPage = document.getElementById("locationMapPage");
+            var contactPage = document.getElementById("contactPage");
+            menu.style.display="none";
+            gamePage.style.display="none";
+            gameDetailsPage.style.display="none";
+            locationsPage.style.display="none";
+            mapPage.style.display="block";
+            contactPage.style.display="none";
+            app.changeID();
+            
+        },
+        changeID: function(){
+        var locationID = document.getElementById("location.name");
+        if(locationID == "AJ Katzenmaier"){
+            this.sourceUrl == this.data2[0].url;
+        }else if(locationID == "Greenbay"){
+            this.sourceUrl == this.data2[1].url;
+        }else if(locationID == "Howard A Yeager"){
+            this.sourceUrl == this.data2[2].url;
+        }else if(locationID == "Marjorie P Hart"){
+            this.sourceUrl == this.data2[3].url;
+        }else if(locationID == "North"){
+            this.sourceUrl == this.data2[4].url;
+        }else if(locationID == "South"){
+            this.sourceUrl == this.data2[5].url;
+        }
+        
     }
     }
 })
